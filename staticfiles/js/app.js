5e2d6204f6ccfce06428f654b02bdf10c2d0e9ee
derickddo <dansoderrick80@yahoo.com>
2023-09-05 18:26:34 +0000

    Modification

--- staticfiles/js/app.js
+++ staticfiles/js/app.js
@@ -5,6 +5,7 @@ let content_menu = document.querySelector('.menu-content')
 let dropdown_content = document.querySelector('.on-mobile')
 let main_menu = document.querySelector('.main-menu')
 let cancel = document.querySelector('.cancel')
+let dotted_menu = document.querySelectorAll('.dotted_menu')
 let dotted_menu_content = document.querySelectorAll('.dotted_menu_content')
 
 menu.addEventListener("click", ()=> {
@@ -17,6 +18,13 @@ menu.addEventListener("click", ()=> {
 category_btn.addEventListener("click", ()=> {
     console.log('hello')
     dropdown_content.classList.toggle('clicked')
+})
+
+dotted_menu.forEach(item =>{
+    item.addEventListener('click', ()=>{
+        let content = item.children.item(3)
+        content.classList.toggle('dotted_menu_content')
+    })
 })
 
 
